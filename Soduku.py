cbf5817823f90ebfc6f9ef58ac222cbecd1124d3
Lockie <richter.lockie@gmail.com>
2019-07-11 23:39:12 +0930

    Add in board verification and unit testing to cover this.

--- Soduku.py
+++ Soduku.py
@@ -1,6 +1,11 @@
+from numpy import array
+from numpy import unique
+
+
 class Soduku:
+
     def __init__(self, board):
-        self.board = board
+        self.board = array(board)
 
     def print_board(self):
         for i in range(9):
@@ -19,3 +24,48 @@ class Soduku:
 
     def get_board(self):
         return self.board
+
+    def verify_row(self, row):
+        row_values = self.board[row][:]
+        if len(row_values) > len(set(row_values)):
+            return False
+        else:
+            return True
+
+    def verify_column(self, column):
+        column_values = self.board[:][column]
+        if len(column_values) > len(set(column_values)):
+            return False
+        else:
+            return True
+
+    def verify_box(self, box):
+        d = box // 3
+        r = box % 3
+        box_values = None
+
+        if d == 0:
+            box_values = self.board[:, :3]
+        elif d == 1:
+            box_values = self.board[:, 3:6]
+        elif d == 2:
+            box_values = self.board[:, 6:]
+
+        if r == 0:
+            box_values = box_values[:3, :]
+        elif r == 1:
+            box_values = box_values[3:6, :]
+        elif r == 2:
+            box_values = box_values[6:, :]
+
+        if len(unique(box_values)) != 9:
+            return False
+        else:
+            return True
+
+    def verify_board(self):
+        for i in range(9):
+            if not (self.verify_row(i) and self.verify_column(i) and self.verify_box(i)):
+                return False
+            else:
+                return True
--- test_Soduku.py
+++ test_Soduku.py
@@ -1,16 +1,74 @@
 from Soduku import Soduku
+from numpy import array
+
+valid_board = [[2, 4, 8, 3, 9, 5, 7, 1, 6],
+               [5, 7, 1, 6, 2, 8, 3, 4, 9],
+               [9, 3, 6, 7, 4, 1, 5, 8, 2],
+               [6, 8, 2, 5, 3, 9, 1, 7, 4],
+               [3, 5, 9, 1, 7, 4, 6, 2, 8],
+               [7, 1, 4, 8, 6, 2, 9, 5, 3],
+               [8, 6, 3, 4, 1, 7, 2, 9, 5],
+               [1, 9, 5, 2, 8, 6, 4, 3, 7],
+               [4, 2, 7, 9, 5, 3, 8, 6, 1]]
+
+invalid_board = [[1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
 
-def test_soduku():
-    board_in = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9]]
-    soduku = Soduku(board_in)
+def test_init_with_valid_board():
+    soduku = Soduku(valid_board)
     board_out = soduku.get_board()
-    assert board_in == board_out
+    assert (array(valid_board) == board_out).all()
+
+
+def test_verify_row_with_valid_rows():
+    soduku = Soduku(valid_board)
+    for i in range(9):
+        assert soduku.verify_row(i)
+
+
+def test_verify_row_with_invalid_rows():
+    soduku = Soduku(invalid_board)
+    for i in range(9):
+        assert not soduku.verify_row(i)
+
+
+def test_verify_column_with_valid_columns():
+    soduku = Soduku(valid_board)
+    for i in range(9):
+        assert soduku.verify_column(i)
+
+
+def test_verify_column_with_invalid_columns():
+    soduku = Soduku(invalid_board)
+    for i in range(9):
+        assert not soduku.verify_column(i)
+
+
+def test_verify_box_with_valid_boxes():
+    soduku = Soduku(valid_board)
+    for i in range(9):
+        assert soduku.verify_box(i)
+
+
+def test_verify_box_with_invalid_boxes():
+    soduku = Soduku(invalid_board)
+    for i in range(9):
+        assert not soduku.verify_box(i)
+
+
+def test_verify_board_with_valid():
+    soduku = Soduku(valid_board)
+    assert soduku.verify_board()
+
+
+def test_verify_board_with_invalid():
+    soduku = Soduku(invalid_board)
+    assert not soduku.verify_board()
