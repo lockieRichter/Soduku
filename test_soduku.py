5820bcfc855afdf8d99ce55ecfe8cad8eee73539
lockieRichter <richter.lockie@gmail.com>
2019-07-16 21:29:31 +0930

    Create get_box(row, col) method and unit tests in soduku class.

--- test_soduku.py
+++ test_soduku.py
@@ -26,3 +26,28 @@ def test_add_cell_with_invalid():
     value = 's'
     with pytest.raises(Exception):
         soduku_board.add_cell(row, column, value)
+
+
+def test_get_box_with_valid():
+    soduku_board = Soduku(boards.initial)
+
+    box1 = soduku_board.get_box(0, 0)
+    expected_box1 = array([[5, 3, 0], [6, 0, 0], [0, 9, 8]])
+    assert box1.all() == expected_box1.all()
+
+    box2 = soduku_board.get_box(1, 3)
+    expected_box2 = array([[0, 7, 0], [1, 9, 5], [0, 0, 0]])
+    assert box2.all() == expected_box2.all()
+
+    box6 = soduku_board.get_box(3, 8)
+    expected_box6 = array([[0, 7, 0], [1, 9, 5], [0, 0, 0]])
+    assert box6.all() == expected_box6.all()
+
+    box7 = soduku_board.get_box(8, 2)
+    expected_box7 = array([[0, 7, 0], [1, 9, 5], [0, 0, 0]])
+    assert box7.all() == expected_box7.all()
+
+    box9 = soduku_board.get_box(6, 6)
+    expected_box9 = array([[0, 7, 0], [1, 9, 5], [0, 0, 0]])
+    assert box9.all() == expected_box9.all()
+
